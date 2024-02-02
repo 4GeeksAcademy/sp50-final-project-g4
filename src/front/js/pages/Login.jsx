a4bb7c317216d097e1ff558ce1e3a5e4c6dec9c1
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-02-02 18:10:13 +0000

    feat: Majo Login nueva rama

---
+++ src/front/js/pages/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState } from "react";
+import login from "../../styles/login.css"
+import LogoURL from "../../img/BabySteps.png";
+import { Context } from "../store/appContext";
+
+
+export const Login = () => {
+    const { store, actions } = useContext(Context)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = (e) => {
+        e.preventDefault();
+        actions.login(email, password)
+    };
+
+    return (
+        <div class="wrapper">
+            <div class="login-box" >
+                <div class="login-header" >
+                    <img className = "" src={LogoURL}       />
+                </div>
+                <form onSubmit={e => login(e)}>
+                    <div class="input-box">
+                        <input type="text" id="user" class="input-field" onChange={e => setEmail(e.target.value)} placeholder="email" required />
+
+
+                        <i class="bx bx-user icon"></i>
+                    </div>
+                    <div class="input-box">
+                        <input type="password" id="pass" class="input-field" onChange={e => setPassword(e.target.value)} autocomplete="off" placeholder="password" />
+                        <i class="bx bx-lock-alt icon" id="show-password">
+                        </i>
+                    </div>
+
+                    <div class="input-box">
+                        <input type="submit" class="input-submit" value="Accede" style={{ backgroundColor: "#086972" }} />
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    );
+};
