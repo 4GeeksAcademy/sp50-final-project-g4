84d393419bfb40125f91b254bdbc5bac53632975
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-02-02 20:44:50 +0100

    Revert "Navbar 2.0"

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 export const Navbar = () => {
 	return (
-		<>
-		{
-			localStorage.getItem("token") ?
-		
-				<nav className="navbar navbar-light bg-light">
-					<div className="container">
-						<Link to="/">
-							<span className="navbar-brand mb-0 h1">BabySteps</span>
-						</Link>
-						<div className="ml-auto">
-							<Link to="/demo">
-								<button className="btn btn-primary" style={{ backgroundColor: "#086972" }}>Cerrar Sesión</button>
-							</Link>
-						</div>
-					</div>
-				</nav>
-				:
-				""
-		}
-	</>
-	)
+		<nav className="navbar navbar-light bg-light">
+			<div className="container">
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				</Link>
+				<div className="ml-auto">
+					<Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link>
+				</div>
+			</div>
+		</nav>
+	);
 };
