0fa7908edd5fada2c0aef1d82860ef1a189a2613
Luis Alejandro Bermudez <142993857+Alejobermudezb@users.noreply.github.com>
2024-02-05 18:40:51 +0000

    feat:juntando cosas

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -10,6 +10,9 @@ import ScrollToTop from "./component/ScrollToTop.jsx";
 import { BackendURL } from "./component/BackendURL.jsx";
 import { Navbar } from "./component/Navbar.jsx";
 import { Footer } from "./component/Footer.jsx";
+import Card from "./component/Card.jsx";
+import { Cards } from "./component/Cards.jsx";
+import { Login } from "./pages/Login.jsx";
 
 
 // Create your first component
@@ -30,6 +33,7 @@ const Layout = () => {
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} path="*"/>
                         <Route element={<Cards />} path="/card" />
+                        <Route element={<Login />} path="/login" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
