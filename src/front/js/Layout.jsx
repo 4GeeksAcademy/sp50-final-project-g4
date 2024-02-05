9937807b0c669311ad2ce7fa6fd1172158272f54
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-02-05 19:26:49 +0100

    Revert "Navbar 3.0"

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -10,9 +10,6 @@ import ScrollToTop from "./component/ScrollToTop.jsx";
 import { BackendURL } from "./component/BackendURL.jsx";
 import { Navbar } from "./component/Navbar.jsx";
 import { Footer } from "./component/Footer.jsx";
-import Card from "./component/Card.jsx";
-import { Cards } from "./component/Cards.jsx";
-import { Login } from "./pages/Login.jsx";
 
 
 // Create your first component
@@ -32,7 +29,6 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} path="*"/>
-                        <Route element={<Cards />} path="/card" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+    <footer className="footer mt-auto py-3 text-center" style={{ backgroundColor: "#E6E6E6" }} >
+        <p>
+            Made with <i className="fa fa-heart text-danger" /> by Ale, Dani y Majo.
+        </p>
+    </footer>
 );
