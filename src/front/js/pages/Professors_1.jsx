af8a7d0bd4941a6d50d212e76c48451427bc8338
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-02-10 09:53:57 +0000

    Ft Majo muy basico todas las opciones listas

--- src/front/js/pages/Professors_1.jsx
+++ src/front/js/pages/Professors_1.jsx
@@ -12,14 +12,14 @@ export const Professors_1 = () => {
         <div className="Container-1">
             <div className="SubContainer-1">
                 <div className="Title">
-                    <h1>Bienvenido "store.user.name"</h1>
+                    <h2>Bienvenido "store.user.name"</h2>
                 </div>
             </div>
             <div class="CardsProfessors">
                 <div class="card_StudentsList">
                     <img src="..." class="card-img-top" alt="..." />
                     <div class="cardList">
-                        <h5>ALUMNOS DEL GRUPO</h5>
+                        <h2>ALUMNOS DEL GRUPO</h2>
                         <p class="card-text">De aquí me traigo la lista de alumnos</p>
                         <ul class="list-group">
                             <li class="list-group-item">Alumno 1</li>
@@ -31,21 +31,22 @@ export const Professors_1 = () => {
                     </div>
                 </div>
                 <div class="card_StudentsNotif">
-                    <div class="food">
+                    <h1>NOTIFICACIONES</h1>
+                    <div class="Food">
                         <h2 class="food-header">
-                            Notificaciones Alimentación <span className="acordion_icons"><i class="fa-solid fa-utensils"></i></span>
+                            Notificaciones Alimentación <span className="notif_icons"><i class="fa-solid fa-utensils"></i></span>
                         </h2>
-                        <div class="Food">
+                        <div class="FoodCheck">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaulta" />
-                                <label class="form-check-label" for="flexRadioDefaulta">
+                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultFood" />
+                                <label class="form-check-label" for="flexRadioDefaultfood">
                                     Come bien
                                 </label>
 
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultb" />
-                                <label class="form-check-label" for="flexRadioDefaultb">
+                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultFood" />
+                                <label class="form-check-label" for="flexRadioDefaultFood">
                                     Come poco
                                 </label>
 
@@ -58,11 +59,55 @@ export const Professors_1 = () => {
 
                             </div>
                         </div>
+                    </div>
+                    <div class="Sleep">
+                        <h2 class="sleep-header">
+                            Notificaciones Sueño <span className="notif_icons"><i class="fa-solid fa-utensils"></i></span>
+                        </h2>
+                        <div class="SleepCheck">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultSleep" />
+                                <label class="form-check-label" for="flexRadioDefaultSleep">
+                                    Duerme
+                                </label>
 
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultSleep" />
+                                <label class="form-check-label" for="flexRadioDefaultSleep">
+                                    No duerme
+                                </label>
+
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="Hygiene">
+                        <h2 class="hygiene-header">
+                            Notificaciones Higiene <span className="notif_icons"><i class="fa-solid fa-poop"></i></span>
+                        </h2>
+                        <div class="HygieneCheck">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultHygiene" />
+                                <label class="form-check-label" for="flexRadioDefaultHygiene">
+                                    Cambio
+                                </label>
+
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="Come" id="flexRadioDefaultHygiene" />
+                                <label class="form-check-label" for="flexRadioDefaultHygiene">
+                                    No cambio
+                                </label>
+
+                            </div>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
 
 
     );
