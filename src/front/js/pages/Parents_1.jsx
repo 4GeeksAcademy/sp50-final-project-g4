8871356486f80c75022fe59f99d8e3a0d46f1a93
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-02-12 13:14:06 +0000

    ft majo parents

--- src/front/js/pages/Parents_1.jsx
+++ src/front/js/pages/Parents_1.jsx
@@ -1,32 +1,89 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import basics from "../../styles/basics.css"
+// import basics from "../../styles/basics.css"
 import MenuURL from "../../img/MenuBS.jpg"
 import CalendarioURL from "../../img/Calendario.jpg"
 
-export const Professors_1 = () => {
+export const Parents_1 = () => {
     const { store, actions } = useContext(Context);
 
 
     return (
-        <div className="Container-1">
-            <div className="SubContainer-1">
+        <div className="Container-fr">
+            <div className="SubContainer-frrf">
                 <div className="Title">
                     <h2>Bienvenido "store.user.name"</h2>
                 </div>
             </div>
-            <div className="TabsMenuCalendar">
-                <ul class="nav justify-content-center">
+            <div className="TabsMenuCalendar" style={{ display: "grid" }}>
+                {/* <ul class="nav justify-content-center">
                     <li class="nav-item-calendar">
                         <a class="nav-link active" aria-current="page" href="#">Calendario Escolar</a>
                     </li>
                     <li class="nav-item-menu">
                         <a class="nav-link" href="#">Menú Mensual</a>
                     </li>
-                </ul>
+                </ul> */}
+
+                {/* Boton 1 Calendario  */}
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style={{
+    marginRight: "10px",
+    marginLeft: "10px",
+  }}>
+                    Calendario
+                </button>
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Calendario 2023 -2024</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img
+                                    className="ImagenPadres"
+                                    src={CalendarioURL}
+                                    style={{ width: "85%", height: "85%" }}
+                                />
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* Boton 2 Menú   */}
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style={{
+    marginRight: "10px",
+    marginLeft: "10px",
+  }}>
+                    Menú Mensual
+                </button>
+                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Menú Febrero 2024</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img
+                                    className="ImagenPadres"
+                                    src={MenuURL}
+                                    style={{ width: "85%", height: "85%" }}
+                                />
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="CardNotif">
-                <div class="card" style="width: 18rem;">
+                <div class="card" >
                     <div class="card-body">
                         <h5 class="card-title">"store.student.name"</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Notificaciones del día</h6>
